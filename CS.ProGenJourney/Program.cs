65daef3b1a084672027af2c69ab8b07c6c17b2ab
nubowski <nubowski@nnteam.ru>
2023-06-23 16:43:36 +0300

    feat: minimum output tryout (codename: `Kolhoz`)

--- CS.ProGenJourney/Program.cs
+++ CS.ProGenJourney/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using CS.ProGenJourney.Controllers;
+using CS.ProGenJourney.Models;
+
+static void Main(string[] args)
+{
+    TerrainController terrainController = new TerrainController(16);
+    List<TerrainPoint> terrain = terrainController.GenerateTerrain(80, 30);
+    terrainController.ApplyPerlinNoise(terrain, 8.0);
+    terrainController.PrintTerrain(terrain, 80, 30);
+    Console.ReadLine();
+}
